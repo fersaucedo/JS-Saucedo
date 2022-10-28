348c1ca77c70b63da1c5122737e01ec5568e730a
Fernando Saucedo <frsauce@gmail.com>
2022-10-27 22:00:11 -0300

    version1

--- main.js
+++ main.js
@@ -1,33 +1,38 @@
+let mensaje = alert('Hola! Esto es un simulador de calculadora. Por favor, complete los siguientes campos.');
+let numero1 = parseFloat(prompt("Por favor, ingrese un número: "));
+let operador = prompt("Ahora ingrese la operación que desea realizar ( '+, '-', 'x', '/' ): ");
+let numero2 = parseFloat(prompt("En el siguiente campo, ingrese un segundo número: "));
+let resultado = 0;
 
 function calcular() {
 
-    let numero1 = parseFloat(prompt("Ingresar primer numero: "));
-    let operador = prompt("ingresar operacion (+, -, x, /): ");
-    let numero2 = parseFloat(prompt("Ingresar segundo numero: "));
-    let resultado = 0;
-
-
     if (operador === '+') {
         resultado = numero1 + numero2;
-        alert("El resultado es : " + resultado);
+        alert("El resultado de su cuenta es : " + resultado);
     } else if (operador === '-') {
         resultado = numero1 - numero2;
-        alert("El resultado es : " + resultado);
-    } else if (operador ==='x') {
+        alert("El resultado de su cuenta es : " + resultado);
+    } else if (operador ==='x' || operador === 'X') {
         resultado = numero1 * numero2;
-        alert("El resultado es : " + resultado);
+        alert("El resultado de su cuenta es : " + resultado);
     } else if (operador === '/') {
         resultado = numero1 / numero2;
-        alert("El resultado es : " + resultado);
+        alert("El resultado de su cuenta es : " + resultado);
     } else {
         alert("Error al ingresar un operador. Por favor, intentelo nuevamente ingresando '+, '-', 'x', '/'");
     }    
 };
 
+
 do {
     calcular();
     continuar = confirm("Desea realizar otra operacion?");
-} while (continuar)
+} while (continuar);
+
+
+
+
+
 
 
 
