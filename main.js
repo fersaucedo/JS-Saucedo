53fdb3a2605e4b881f0bac37be17e911c8e90685
Fernando Saucedo <frsauce@gmail.com>
2022-10-27 22:08:50 -0300

    version1

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 let mensaje = alert('Hola! Esto es un simulador de calculadora. Por favor, complete los siguientes campos.');
-let numero1 = parseFloat(prompt("Por favor, ingrese un número: "));
-let operador = prompt("Ahora ingrese la operación que desea realizar ( '+, '-', 'x', '/' ): ");
-let numero2 = parseFloat(prompt("En el siguiente campo, ingrese un segundo número: "));
-let resultado = 0;
 
-function calcular() {
+function calcular() { 
+
+    let numero1 = parseFloat(prompt("Por favor, ingrese un número: "));
+    let operador = prompt("Ahora ingrese la operación que desea realizar ( '+, '-', 'x', '/' ): ");
+    let numero2 = parseFloat(prompt("En el siguiente campo, ingrese un segundo número: "));
+    let resultado = 0;
 
     if (operador === '+') {
         resultado = numero1 + numero2;
@@ -20,9 +21,8 @@ function calcular() {
         alert("El resultado de su cuenta es : " + resultado);
     } else {
         alert("Error al ingresar un operador. Por favor, intentelo nuevamente ingresando '+, '-', 'x', '/'");
-    }    
+    }
 };
-
 
 do {
     calcular();
