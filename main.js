217e6b6c384217540f7943da2b9fa0a09d132ae3
Fernando Saucedo <frsauce@gmail.com>
2022-11-11 21:47:04 -0300

    nuevo simulador

--- main.js
+++ main.js
@@ -1,57 +1,110 @@
-let mensaje = alert('Hola! Esto es un simulador de calculadora. Por favor, complete los siguientes campos.');
-let numero1 = 0;
-let operador = '';
-let numero2 = 0;
-let resultado = 0;
-
-function calcular() {
-
-    if (operador === '+') {
-        resultado = numero1 + numero2;
-        alert("El resultado de su cuenta es : " + resultado);
-    } else if (operador === '-') {
-        resultado = numero1 - numero2;
-        alert("El resultado de su cuenta es : " + resultado);
-    } else if (operador === 'x' || operador === 'X') {
-        resultado = numero1 * numero2;
-        alert("El resultado de su cuenta es : " + resultado);
-    } else if (operador === '/') {
-        resultado = numero1 / numero2;
-        alert("El resultado de su cuenta es : " + resultado);
-    } else {
-        alert("Error al ingresar un operador. Por favor, intentelo nuevamente ingresando '+', '-', 'x', '/'");
+const articulos = [{
+        id: '1',
+        nombre: 'Skate',
+        precio: 15000
+    },
+    {
+        id: '2',
+        nombre: 'Longboard',
+        precio: 20000
+    },
+    {
+        id: '3',
+        nombre: 'Surf Skate',
+        precio: 13000
     }
 
+];
+
+
+function mostrarListado() {
+    const listado = confirm('A continuacion, se mostrará la lista de articulos a la venta y sus montos. Desea mostrarla en orden del menor precio al mayor?');
+    if (listado) {
+        ascendente();
+    } else {
+        descendente();
+    }
+
+    comprar();
+};
+
+
+const ascendente = () => {
+    articulos.sort((a, b) => a.precio - b.precio);
+    listadoOrdenado();
+};
+
+
+const descendente = () => {
+    articulos.sort((a, b) => b.precio - a.precio);
+    listadoOrdenado();
+};
+
+const listadoOrdenado = () => {
+    let array = [];
+    articulos.forEach(producto => array.push(producto.nombre + ' $' + producto.precio));
+    alert('Lista de precios:' + '\n' + array.join('\n'));
+};
+
+
+function comprar() {
+    let articulo = '';
+    let cantidad = 0;
+    let total = 0;
+
+
+    do {
+        articulo = prompt('Que tabla desea comprar? Ingrese: 1 si desea comprar un SKATE. Ingrese: 2 si desea comprar un LONGBOARD. Ingrese: 3 si desea comprar un SURF SKATE.', 'Ingrese el articulo');
+
+        const producto = articulos.find(producto => producto.id === articulo);
+
+        cantidad = parseInt(prompt('Por favor ingrese la cantidad que desea comprar:'));
+
+        let cantidadValidada = validarCantidad(cantidad);
+
+
+        if (producto) {
+            total += producto.precio * cantidadValidada;
+            alert('El subtotal de la compra es:' + total);
+        } else {
+            alert('El producto no se encuentra en stock.');
+        };
+
+        seguirComprando = confirm('Desea agregar otro producto?');
+
+    } while (seguirComprando);
+
+    calcularEnvio(total);
+};
+
+function validarCantidad(cantidad) {
+    while (Number.isNaN(cantidad) || cantidad === 0) {
+        if (cantidad !== 0) {
+            alert("ERROR! No se ha ingresado una cantidad");
+        } else {
+            alert('Debe ingresar un número distinto de cero.')
+        }
+        cantidad = parseInt(prompt('Por favor ingrese la cantidad que desea comprar:'));
+    }
+
+    return cantidad;
 }
 
+function calcularEnvio(totalCompra) {
+    let envio = confirm('Desea calcular el costo de envio?');
 
-function validador1() {
-    while (Number.isNaN(numero1)) {
-        alert("ERROR! No se ha ingresado un número");
-        numero1 = parseFloat(prompt("Por favor, ingrese nuevamente el número: ")); 
-    } 
+    if (envio && totalCompra >= 15000) {
+        alert('Como su compra supera los $15000 ,el envio es gratis! El total de la compra es: ' + totalCompra);
+    } else if (envio && totalCompra < 15000 && totalCompra !== 0) {
+        totalCompra += 580;
+        alert('El costo de envío es de $580. El total de la compra es: ' + totalCompra);
+    } else {
+        alert('El costo total de la compra es: ' + totalCompra);
+    }
 };
 
-function validador2() {
-    while (Number.isNaN(numero2)) {
-        alert("ERROR! No se ha ingresado un número");
-        numero2 = parseFloat(prompt("Por favor, ingrese nuevamente el segundo número: ")); 
-    } 
-};
 
 
+let mensaje = alert('Bienvenido! Esto es un simulador de carrito de compras.');
 
-do {
-    numero1 = parseFloat(prompt("Por favor, ingrese un número: "));
-    validador1(numero1);
-
-    operador = prompt("Ahora ingrese la operación que desea realizar ( '+', '-', 'x', '/' ): ");
-
-    numero2 = parseFloat(prompt("En el siguiente campo, ingrese un segundo número: "));
-    validador2(numero2);
-
-    calcular();
-
-    continuar = confirm("Desea realizar otra operacion?");
-    
-} while (continuar);
+mostrarListado();
